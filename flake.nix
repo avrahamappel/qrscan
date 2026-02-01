c79df87024a35fecafc876e242f4bc623150b674
Avraham Appel <aappel248@gmail.com>
2026-01-31 22:54:32 -0500

    Use commit hash for version string

--- flake.nix
+++ flake.nix
@@ -31,11 +31,7 @@
       packages = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
-          version = builtins.concatStringsSep "-" [
-            (builtins.substring 0 4 self.lastModifiedDate)
-            (builtins.substring 4 2 self.lastModifiedDate)
-            (builtins.substring 6 2 self.lastModifiedDate)
-          ];
+          version = if self ? shortRev then self.shortRev else "dirty";
         in
         {
           qrscan = pkgs.rustPlatform.buildRustPackage {
